995b701ead3d14bddfa6168423f4415bc8741def
Yuri Duarte <yuriduarte314@gmail.coml>
2019-02-22 20:26:18 -0300

    Altera nome da migrate

--- db/migrate/20190221162515_add_link_youtube_to_tanks.rb
+++ db/migrate/20190221162515_add_link_youtube_to_tanks.rb
@@ -1,4 +1,4 @@
-class AddFieldsToTanks < ActiveRecord::Migration[5.0]
+class AddLinkYoutubeToTanks < ActiveRecord::Migration[5.0]
   def change
     add_column :tanks, :link_libras, :string
     add_column :tanks, :link_audio, :string
